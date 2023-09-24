de2cd93feb3c9ddbe5639ca7da91bb561307ab15
Guillaume Blanc <guillaumeblanc.sc@gmail.com>
2023-09-24 13:59:02 +0200

    Tweaks touchpoint settings

--- src/impl/orbit_camera.h
+++ src/impl/orbit_camera.h
@@ -20,10 +20,10 @@ class OrbitCamera : public Camera {
   }
 
  private:
-  bool Orbit(float _dx, float _dy);
+  bool Orbit(const HMM_Vec2& _delta);
   bool Zoom(float _d);
-  bool Pan(float _dx, float _dy);
-  bool Move(float _dx, float _dy);
+  bool Pan(const HMM_Vec2& _delta);
+  bool Move(const HMM_Vec2& _delta);
 
   HMM_Vec2 last_touches_[SAPP_MAX_TOUCHPOINTS];
 
@@ -32,11 +32,11 @@ class OrbitCamera : public Camera {
   const float kMinLat = -kPi_2 * .95f;
   const float kMaxLat = kPi_2 * .95f;
   const float kOrbitFactor = .01f;
-  const float kOrbitTouchFactor = .3f;
+  const float kOrbitTouchFactor = .005f;
   const float kPanFactor = .002f;
-  const float kPanTouchFactor = .05f;
+  const float kPanTouchFactor = .01f;
   const float kMoveFactor = .002f;
-  const float kZoomFactor = .5f;
+  const float kZoomFactor = .2f;
   const float kZoomTouchFactor = .02f;
 
   float distance_ = 7.f;
